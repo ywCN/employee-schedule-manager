fd7a584aec6a5c7d22f330e3e54c872c87d67d40
ywCN <wangyouhao90@gmail.com>
2017-12-16 18:09:23 -0500

    import things

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 
