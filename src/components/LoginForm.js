b98eeb957413d1a07cb88645ecbcda685d948928
ywCN <wangyouhao90@gmail.com>
2017-12-17 10:38:40 -0500

    build button/spinner helper

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
 
 class LoginForm extends Component {
@@ -33,6 +33,17 @@ class LoginForm extends Component {
         }
     }
 
+    renderButton() { // we do not need button all the time for Spinner
+        if (this.props.loading) {
+            return <Spinner size='large' />;
+        }
+
+        return (
+        <Button onPress={this.onButtonPress.bind(this)}>
+            Login
+        </Button>);
+    }
+
     render() {
         return (
             <Card>
@@ -58,9 +69,7 @@ class LoginForm extends Component {
                 {this.renderError()}
 
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Login
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
