21584c5240760afb75f2f1c60c78d365cc142b3a
ywCN <wangyouhao90@gmail.com>
2017-12-17 18:57:45 -0500

    complete reducer

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -6,7 +6,12 @@ const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case EMPLOYEE_UPDATE:
+            return { ...state, [action.payload.prop]: action.payload.value };
+            // [action.payload.prop]: action.payload.value
+            // is ES6 key interpolation
+            // it can become: name, shift, or ...
         default:
-        return state;
+            return state;
     }
 };
