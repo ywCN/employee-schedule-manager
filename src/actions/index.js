70e6ebaf920c457b645ad1852a5eeeae2e781c23
ywCN <wangyouhao90@gmail.com>
2017-12-17 10:01:44 -0500

    refactor

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,14 +23,10 @@ export const loginUser = ({ email, password }) => { // this pattern is for asyn 
     // return a function, so Redux will immediate call the function
     return (dispatch) => { // since we have dispatch, we can dispatch many actions
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(user => { // manually dispatch
-                dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
-            })
+            .then(user => loginUserSuccess(dispatch, user))
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .then(user => {
-                        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
-                    });
+                    .then(user => loginUserSuccess(dispatch, user));
             });
     };  
 };
