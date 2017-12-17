a04553093a0a0e2e584608cc18758054b06bb263
ywCN <wangyouhao90@gmail.com>
2017-12-16 19:31:13 -0500

    return a new object

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,7 +5,13 @@ const INITIAL_STATE = { email: '' };
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case EMAIL_CHANGED:
-            
+            // This assignment will not create new state
+            // so old===new, redux will not record change
+            // state.email = action.payload;
+            // return state;
+
+            // This will create a new object
+            return { ...state, email: action.payload };
         default:
             return state;
     }
