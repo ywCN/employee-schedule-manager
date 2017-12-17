fa3d2ae4cdb13a174254988ef7779bd306269bb0
ywCN <wangyouhao90@gmail.com>
2017-12-17 18:32:26 -0500

    build structure

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -4,7 +4,19 @@ import { Card, CardSection, Input, Button } from './common';
 class EmployeeCreate extends Component {
     render() {
         return (
-            
+            <Card>
+                <CardSection>
+                </CardSecton>
+
+                <CardSection>
+                </CardSecton>
+
+                <CardSection>
+                </CardSecton>
+
+                <CardSection>
+                </CardSecton>
+            </Card>
         );
     }
 }
