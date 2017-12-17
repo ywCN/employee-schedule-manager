f61a26f556b8663d1aeaac2ceefb404866aaf7c7
ywCN <wangyouhao90@gmail.com>
2017-12-17 18:20:53 -0500

    wired

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
@@ -19,7 +19,7 @@ const RouterComponent = () => {
                     title='Employees'
 
                     rightTitle='Add'
-                    onRight={() => console.log('right!!')}
+                    onRight={() => Actions.employeeCreate()}
                 />
 
                 <Scene key='employeeCreate' title='Create Employee' />
