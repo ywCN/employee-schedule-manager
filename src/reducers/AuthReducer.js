db05f4a713ef5fc127869beff992acfc21569814
ywCN <wangyouhao90@gmail.com>
2017-12-17 10:33:13 -0500

    toggle login status

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -28,13 +28,13 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, password: action.payload };
 
         case LOGIN_USER:
-            return { ...state, loading: true };
+            return { ...state, loading: true, error: '' };
 
         case LOGIN_USER_SUCCESS:
-            return { ...state, user: action.payload, error: '' };
+            return { ...state, user: action.payload, error: '', loading: false };
 
         case LOGIN_USER_FAIL: // can also reset password here by password=''
-            return { ...state, error: 'Authentication Failed.' };
+            return { ...state, error: 'Authentication Failed.', loading: false };
 
         default:
             return state;
