358756af2db226ec33a25cb5afb0230b627fdcbd
ywCN <wangyouhao90@gmail.com>
2017-12-17 15:44:40 -0500

    add init scene

--- src/Router.js
+++ src/Router.js
@@ -7,7 +7,7 @@ const RouterComponent = () => {
     return (
         <Router sceneStyle={{ paddingTop: 65 }}>
             <Scene key='login' component={LoginForm} title='Please Login' />
-            <Scene key='employeeList' component={EmployeeList} title='Employees' />
+            <Scene key='employeeList' component={EmployeeList} title='Employees' initial />
         </Router>
     );
 };
