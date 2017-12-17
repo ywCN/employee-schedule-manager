b69de46ad872f6e636ace2ccf210c6059d416be9
ywCN <wangyouhao90@gmail.com>
2017-12-17 18:31:30 -0500

    ready for creating form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>Employee Form</Text>
-            </View>
+            
         );
     }
 }
