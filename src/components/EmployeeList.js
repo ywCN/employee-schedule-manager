a4d1c963c8489172904ad02d3da421c17d72fe58
ywCN <wangyouhao90@gmail.com>
2017-12-17 15:41:31 -0500

    create some dummy text

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>hhhhhhh</Text>
+                <Text>hhhhhhh</Text>
+                <Text>hhhhhhh</Text>
+                <Text>hhhhhhh</Text>
+                <Text>hhhhhhh</Text>
+                <Text>hhhhhhh</Text>
+            </View>
+        );
+    }
+}
+
+export default EmployeeList;
