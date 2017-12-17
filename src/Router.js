ffd73360c1a2e6b0659a892ffda1a6687e4a82cb
ywCN <wangyouhao90@gmail.com>
2017-12-17 18:04:47 -0500

    group scenes

--- src/Router.js
+++ src/Router.js
@@ -6,8 +6,13 @@ import EmployeeList from './components/EmployeeList';
 const RouterComponent = () => {
     return ( // initial make the scene the first one to show
         <Router sceneStyle={{ paddingTop: 65 }}>
-            <Scene key='login' component={LoginForm} title='Please Login' />
-            <Scene key='employeeList' component={EmployeeList} title='Employees' />
+            <Scene key='auth'>
+                <Scene key='login' component={LoginForm} title='Please Login' />
+            </Scene>
+
+            <Scene keu='main'>
+                <Scene key='employeeList' component={EmployeeList} title='Employees' />
+            </Scene>
         </Router>
     );
 };
