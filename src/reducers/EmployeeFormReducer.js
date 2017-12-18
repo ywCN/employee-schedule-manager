127f9e800910710c909348c60f2212e8795a3fdf
ywCN <wangyouhao90@gmail.com>
2017-12-18 16:00:02 -0500

    config reducer

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,6 +1,7 @@
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEE_SAVE_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -18,7 +19,9 @@ export default (state = INITIAL_STATE, action) => {
             // is ES6 key interpolation
             // it can become: name, shift, or ...
         case EMPLOYEE_CREATE:
-            return INITIAL_STATE; // reset
+            return INITIAL_STATE; // reset attribute in form
+        case EMPLOYEE_SAVE_SUCCESS:
+            return INITIAL_STATE; // reset attribute in form
         default:
             return state;
     }
