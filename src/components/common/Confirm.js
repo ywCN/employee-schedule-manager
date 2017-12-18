a6f068a5a1626400ea494e9b0f79eabda4d61ae2
ywCN <wangyouhao90@gmail.com>
2017-12-18 16:58:09 -0500

    add styles

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -27,4 +27,22 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
     );
 };
 
+const styles = {
+    cardSelectionStyle: {
+        justifyContent: 'center'
+    },
+    textStyle: {
+        flex: 1,
+        fontSize: 18,
+        textAlign: 'center',
+        lineHeight: 40
+    },
+    containerStyle: {
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        position: 'relative',
+        flex: 1,
+        justifyContent: 'center'
+    }
+};
+
 export { Confirm };
