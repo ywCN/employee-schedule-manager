c1adfdc2e971847b0f089ef400f0064d2db54382
ywCN <wangyouhao90@gmail.com>
2017-12-17 21:24:08 -0500

    callback need to be bind in the context

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -49,7 +49,7 @@ class EmployeeCreate extends Component {
                 </CardSection>
 
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Create
                     </Button>
                 </CardSection>
