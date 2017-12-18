4796804081eeea1dba05bf77f289ccb609798121
ywCN <wangyouhao90@gmail.com>
2017-12-17 23:21:31 -0500

    import for connenction

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
     render() {
