1fb0cd48073c9e5136e7a66edd2ba8a032637653
ywCN <wangyouhao90@gmail.com>
2017-12-17 23:17:04 -0500

    set up action creator

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEE_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -23,5 +24,16 @@ export const employeeCreate = ({ name, phone, shift }) => {
             Actions.employeeList({ type: 'reset' });
         });
         // { type: 'reset' } make the back button disappear
+    };
+};
+
+export const employeeFetch = () => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() });
+            });
     };
 };
