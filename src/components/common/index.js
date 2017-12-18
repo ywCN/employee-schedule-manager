80d97a365987a9504c24e601ce0176ab98e7c54d
ywCN <wangyouhao90@gmail.com>
2017-12-18 16:43:39 -0500

    export

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -4,6 +4,7 @@ export * from './CardSection';
 export * from './Header';
 export * from './Input';
 export * from './Spinner';
+export * from './Confirm';
 
 // with this kind of export, we cannot use 'export default Card' anymore
 // Instead we use 'export { Card: Card }', in ES6 this can be 'export { Card }'
