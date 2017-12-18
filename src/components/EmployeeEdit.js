7d317ee33a6d31e1043fcfc10caa13e880d2ff8d
ywCN <wangyouhao90@gmail.com>
2017-12-18 15:36:22 -0500

    use lodash

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,9 +1,18 @@
+import _ from 'lodash'; // need to iterate all properties of employee
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Button } from './common';
 
 class EmployeeEdit extends Component {
+    componentWillMount() {
+        // for each prop of employee, call action creato with key-value pair
+        _.each(this.props.employee, (value, prop) => {
+            this.props.employeeUpdate({ prop, value });
+        });
+    }
+
     render() {
         return (
             <Card>
@@ -18,4 +27,4 @@ class EmployeeEdit extends Component {
     }
 }
 
-export default connect()(EmployeeEdit);
+export default connect(null, { employeeUpdate })(EmployeeEdit);
