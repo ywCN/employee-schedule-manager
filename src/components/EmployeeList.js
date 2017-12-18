5d1405b1aae4a7ffd8d2ed123e27aed8a6ab96f0
ywCN <wangyouhao90@gmail.com>
2017-12-18 00:19:33 -0500

    always try to create data source

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -6,12 +6,22 @@ import { employeesFetch } from '../actions';
 class EmployeeList extends Component {
     componentWillMount() { // whenever this comp will be show, try to get info
         this.props.employeesFetch();
+        this.createDataSource(this.props);
+    }
 
+    componentWillReceiveProps(nextProps) {
+        // nextProps are te next set of props that this 
+        // component will be rendered with
+        // this.props is still the old set of props
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
         const ds = new ListView.DataSource({
             rowHasChanged: (r1, r2) => r1 !== r2
         });
 
-        this.dataSource = ds.cloneWithRows(this.props.employees);
+        this.dataSource = ds.cloneWithRows(employees);
     }
 
     render() {
