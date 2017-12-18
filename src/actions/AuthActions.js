f30a72214a0289c44e9cb45f23964057dd405d73
ywCN <wangyouhao90@gmail.com>
2017-12-17 19:00:36 -0500

    refactor

---
+++ src/actions/AuthActions.js
@@ -0,0 +1,56 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import {
+    EMAIL_CHANGED,
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL,
+    LOGIN_USER
+} from './types';
+
+export const emailChanged = (text) => {
+    return {
+        type: EMAIL_CHANGED,
+        payload: text
+    };
+};
+
+export const passwordChanged = (text) => {
+    return {
+        type: PASSWORD_CHANGED,
+        payload: text
+    };
+};
+
+export const loginUser = ({ email, password }) => { // this pattern is for asyn action
+    // return a function, so Redux will immediate call the function
+    return (dispatch) => { // since we have dispatch, we can dispatch many actions
+        dispatch({ type: LOGIN_USER });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(user => loginUserSuccess(dispatch, user))
+            .catch((error) => {
+                console.log(error); // helps find bug in AuthReducer
+                
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(user => loginUserSuccess(dispatch, user))
+                    .catch(() => loginUserFail(dispatch));
+            });
+    };  
+};
+
+// helper function for login success
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+
+    Actions.main();
+};
+
+// helper function for login failure
+const loginUserFail = (dispatch) => {
+    dispatch({ type: LOGIN_USER_FAIL });
+};
+
