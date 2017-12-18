acbc608452b8b5ffd890cc5ca9c86f49bad66c84
ywCN <wangyouhao90@gmail.com>
2017-12-17 19:49:51 -0500

    add and create picker and picker options

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Picker } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -30,6 +31,15 @@ class EmployeeCreate extends Component {
                 </CardSection>
 
                 <CardSection>
+                    <Picker>
+                        <Picker.Item label='Monday' value='Monday' />
+                        <Picker.Item label='Tuesday' value='Tuesday' />
+                        <Picker.Item label='Wednesday' value='Wednesday' />
+                        <Picker.Item label='Thursday' value='Thursday' />
+                        <Picker.Item label='Friday' value='Friday' />
+                        <Picker.Item label='Saturday' value='Saturday' />
+                        <Picker.Item label='Sunday' value='Sunday' />
+                    </Picker>
                 </CardSection>
 
                 <CardSection>
