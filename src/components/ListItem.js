a17aeb0eff677bd9fb397b9a363ae93ab1299a22
ywCN <wangyouhao90@gmail.com>
2017-12-18 14:33:53 -0500

    pass empl info

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,8 +4,8 @@ import { Actions } from 'react-native-router-flux';
 import { CardSection } from './common';
 
 class ListItem extends Component {
-    onRowPress() {
-        Actions.employeeCreate(); // this is the form just for testing
+    onRowPress() { // use the same form to edit employee
+        Actions.employeeCreate({ employee: this.props.employee });
     }
 
     render() {
