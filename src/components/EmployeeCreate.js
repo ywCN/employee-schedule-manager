a11f227de436bc2d179f41ea61d2649144cee7e5
ywCN <wangyouhao90@gmail.com>
2017-12-18 14:52:37 -0500

    moved code

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -15,45 +15,6 @@ class EmployeeCreate extends Component {
         // console.log(this.props.employee);
         return (
             <Card>
-                <CardSection>
-                    <Input
-                        label='Name'
-                        placeholder='Jane'
-                        
-                        value={this.props.name}
-                        // onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
-                        onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
-                    />
-                </CardSection>
-
-                <CardSection>
-                    <Input
-                        label='Phone'
-                        placeholder='555-555-5555'
-
-                        value={this.props.phone}
-                        // onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
-                        onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-                    />
-                </CardSection>
-                
-                <CardSection style={{ flexDirection: 'column' }}>
-                    <Text style={styles.pickerTextStyle}>Shift</Text>
-                    <Picker
-                        selectedValue={this.props.shift}
-                        onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
-
-                        // style={{ flex: 1 }}
-                    >
-                        <Picker.Item label='Monday' value='Monday' />
-                        <Picker.Item label='Tuesday' value='Tuesday' />
-                        <Picker.Item label='Wednesday' value='Wednesday' />
-                        <Picker.Item label='Thursday' value='Thursday' />
-                        <Picker.Item label='Friday' value='Friday' />
-                        <Picker.Item label='Saturday' value='Saturday' />
-                        <Picker.Item label='Sunday' value='Sunday' />
-                    </Picker>
-                </CardSection>
 
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -2,7 +2,49 @@ import Reactm { Component } from 'react';
 
 class EmployeeForm extends Component {
     render() {
-        
+        return ( // use View because can only return one element
+            <View>
+                <CardSection>
+                    <Input
+                        label='Name'
+                        placeholder='Jane'
+                        
+                        value={this.props.name}
+                        // onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        label='Phone'
+                        placeholder='555-555-5555'
+
+                        value={this.props.phone}
+                        // onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
+                    />
+                </CardSection>
+                
+                <CardSection style={{ flexDirection: 'column' }}>
+                    <Text style={styles.pickerTextStyle}>Shift</Text>
+                    <Picker
+                        selectedValue={this.props.shift}
+                        onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+
+                        // style={{ flex: 1 }}
+                    >
+                        <Picker.Item label='Monday' value='Monday' />
+                        <Picker.Item label='Tuesday' value='Tuesday' />
+                        <Picker.Item label='Wednesday' value='Wednesday' />
+                        <Picker.Item label='Thursday' value='Thursday' />
+                        <Picker.Item label='Friday' value='Friday' />
+                        <Picker.Item label='Saturday' value='Saturday' />
+                        <Picker.Item label='Sunday' value='Sunday' />
+                    </Picker>
+                </CardSection>
+            </View>
+        );
     }
 }
 
