a40b41dd573b6ebbbda561d82f569b99acbdee8a
ywCN <wangyouhao90@gmail.com>
2017-12-18 15:39:07 -0500

    mapToStateProps

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -12,13 +12,19 @@ class EmployeeEdit extends Component {
             this.props.employeeUpdate({ prop, value });
         });
     }
+    
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+
+        console.log(name, phone, shift);
+    }
 
     render() {
         return (
             <Card>
                 <EmployeeForm />
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Save Changes
                     </Button>
                 </CardSection>
@@ -27,4 +33,10 @@ class EmployeeEdit extends Component {
     }
 }
 
-export default connect(null, { employeeUpdate })(EmployeeEdit);
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+
+    return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
